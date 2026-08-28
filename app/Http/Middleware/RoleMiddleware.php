b1aa59209c397c62fa32f76745adf9d4982fedf1
MisakiZuijin <rtheo3008@gmail.com>
2026-08-28 11:56:37 +0700

    Update Fix Bug Role Middleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
@@ -17,7 +18,7 @@ class RoleMiddleware
         }
 
         if (!$user->is_active) {
-            auth()->logout();
+            Auth::logout();
             $request->session()->invalidate();
             $request->session()->regenerateToken();
             return redirect()->route('login')->withErrors([
